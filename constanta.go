ddcf78e56a5ee02cf6524da3b863af559a98e718
Ihsan Kurniawan <ihsan.1x4n@gmail.com>
2017-12-25 13:44:12 +0700

    add constanta

---
+++ constanta.go
@@ -0,0 +1,13 @@
+package emas
+
+const (
+	VarTransactionCancel 			= 0
+	VarTransactionPending 			= 5
+	VarTransactionDelivering 		= 8
+	VarTransactionSuccess 			= 10
+	VarTransactionDelivered 		= 10
+
+	VarPaymentCancel 				= 0
+	VarPaymentPending 				= 5
+	VarPaymentPaid 					= 10
+)
